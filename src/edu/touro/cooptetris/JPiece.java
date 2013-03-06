40dd7b522f625908edadf8c16f11fd1933e07905
Student <Student@192.168.117.138>
2013-03-05 20:18:24 -0500

    added jpiece, lpiece

---
+++ src/edu/touro/cooptetris/JPiece.java
@@ -0,0 +1,21 @@
+package edu.touro.cooptetris;
+
+import java.awt.Color;
+
+public class JPiece extends Piece {
+
+	public JPiece() {
+		super();
+		for (int i = 0; i < 3; i++) {
+			Square s = squares[i];
+			s.setY(i * s.getSide());
+			s.setColor(Color.BLUE);
+		}
+
+		Square s = squares[3];
+		s.setY(2 * s.getSide());
+		s.setX(s.getX() - s.getSide());
+		s.setColor(Color.BLUE);
+
+	}
+}
