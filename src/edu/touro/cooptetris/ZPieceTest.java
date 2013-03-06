4c12f7d55f3c9994d3536585a028b07d85f53239
hgurwitz <hadassahg@gmail.com>
2013-03-05 20:56:35 -0500

    committing ZPiece and SPiece tests

--- src/edu/touro/cooptetris/ZPieceTest.java
+++ src/edu/touro/cooptetris/ZPieceTest.java
@@ -4,21 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class SPieceTest {
+public class ZPieceTest extends ZPiece {
 
 	@Test
-	public void testSPiece() {
-		SPiece piece = new SPiece();
+	public void testZPiece() {
+		ZPiece piece = new ZPiece();
 		Square[] squares = piece.getSquares();
 		assertEquals(squares[0].getX(), 0);
 		assertEquals(squares[1].getX(), 0);
 		assertEquals(squares[0].getY(), 0);
 		assertEquals(squares[1].getY(), 10);
 		assertEquals(squares[2].getX(), -10);
-		assertEquals(squares[2].getY(), 10);
+		assertEquals(squares[2].getY(), 0);
 		assertEquals(squares[3].getX(), 10);
-		assertEquals(squares[3].getY(), 0);
-
+		assertEquals(squares[3].getY(), 10);
 	}
 
 }
