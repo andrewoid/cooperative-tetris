3b707f4456e3b69bf8ad6e9c11a314420d34555a
akarman <alizak519@gmail.com>
2013-05-19 13:13:17 -0400

    Minor changes- removed unnecessary imports

--- src/edu/touro/cooptetris/net/ClientHandler.java
+++ src/edu/touro/cooptetris/net/ClientHandler.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class ClientHandler extends Thread {
 
