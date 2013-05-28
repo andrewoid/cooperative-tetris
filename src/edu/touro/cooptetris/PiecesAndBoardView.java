f8f0d4f9ae59e3433aa8a435214a94eed6751b66
Chaya <Chaya@chaya-sima>
2013-05-27 20:51:28 -0400

    Made changes in display so score and level can go up to 9999, game ends when score goes above 9999

--- src/edu/touro/cooptetris/PiecesAndBoardView.java
+++ src/edu/touro/cooptetris/PiecesAndBoardView.java
@@ -143,7 +143,7 @@ public class PiecesAndBoardView extends JComponent {
 			}
 			if (landed) {
 				pieces.clear();
-				if (!board.isFull()) {
+				if (!board.isFull() && score < 9999) {
 					pieces.add(pieceFactory.getRandomPiece(Board.NUM_COLUMNS
 							* Square.SIDE / 2, 0));
 					keyListener.setPiece(pieces.get(pieces.size() - 1));
--- src/edu/touro/cooptetris/ScoreLevelDisplay.java
+++ src/edu/touro/cooptetris/ScoreLevelDisplay.java
@@ -7,49 +7,54 @@ import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.border.Border;
 
 public class ScoreLevelDisplay extends JPanel {
 
 	private static final long serialVersionUID = -4724424751560236726L;
 
-	private static JLabel scoreLabel;
-	private static JLabel levelLabel;
+	private static JTextArea scoreTextArea;
+	private static JTextArea levelTextArea;
 	private static String scoreString;
 	private static String levelString;
 
 	public ScoreLevelDisplay() {
+		setSize(100, 300);
+
 		scoreString = "Score: ";
 		levelString = "Level: ";
-		scoreLabel = new JLabel(scoreString + String.valueOf(0));
-		levelLabel = new JLabel(levelString + String.valueOf(1));
-		setSize(100, 300);
-		Border paddingBorder = BorderFactory.createEmptyBorder(15, 15, 15, 15);
+		scoreTextArea = new JTextArea(scoreString + String.valueOf(0));
+		scoreTextArea.setSize(94, 30);
+		scoreTextArea.setLineWrap(true);
+		levelTextArea = new JTextArea(levelString + String.valueOf(1));
+		levelTextArea.setSize(94, 30);
+		levelTextArea.setLineWrap(true);
+		Border paddingBorder = BorderFactory.createEmptyBorder(1, 1, 1, 1);
 		Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
 		Border compoundBorder = BorderFactory.createCompoundBorder(border,
 				paddingBorder);
-		scoreLabel.setBorder(compoundBorder);
-		levelLabel.setBorder(compoundBorder);
+		scoreTextArea.setBorder(compoundBorder);
+		levelTextArea.setBorder(compoundBorder);
 		Font font = new Font("Calibri", Font.BOLD, 18);
-		scoreLabel.setFont(font);
-		levelLabel.setFont(font);
-		setLayout(new GridLayout(5, 1));
+		scoreTextArea.setFont(font);
+		levelTextArea.setFont(font);
+		setLayout(new GridLayout(10, 1));
 		add(new JLabel());
-		add(scoreLabel);
+		add(scoreTextArea);
 		add(new JLabel());
-		add(levelLabel);
-		add(new JLabel());
+		add(levelTextArea);
 
 		setVisible(true);
 	}
 
 	public static void setScore(int score) {
-		scoreLabel.setText(scoreString + String.valueOf(score));
+		scoreTextArea.setText(scoreString + String.valueOf(score));
 
 	}
 
 	public static void setLevel(int level) {
-		levelLabel.setText(levelString + String.valueOf(level));
+		levelTextArea.setText(levelString + String.valueOf(level));
 	}
 
 }
