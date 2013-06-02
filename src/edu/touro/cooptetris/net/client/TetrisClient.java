0160f3a7d676e6238fd427a7030d19661527f60b
Student <Student@192.168.117.42>
2013-06-02 18:55:54 -0400

    made subpackages client and server

---
+++ src/edu/touro/cooptetris/net/client/TetrisClient.java
@@ -0,0 +1,11 @@
+package edu.touro.cooptetris.net.client;
+
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class TetrisClient {
+
+	private Socket socket;
+	private OutputStream out;
+
+}
