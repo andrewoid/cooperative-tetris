13a1d5c16f0f2626a48e4be70ac9a328d8837703
Chaya <Chaya@chaya-sima>
2013-06-13 15:27:50 -0400

    Created CGC test

---
+++ test/edu/touro/cooptetris/net/client/ClientGameControllerTest.java
@@ -0,0 +1,68 @@
+package edu.touro.cooptetris.net.client;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ClientGameControllerTest {
+
+	public ClientGameControllerTest() {
+
+	}
+
+	@Test
+	public void testIncreaseSpeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRotate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveDown() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDrop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLineCompleted() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPauseAndUnPause() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveRow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNewPiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEndGame() {
+		fail("Not yet implemented");
+	}
+
+}
