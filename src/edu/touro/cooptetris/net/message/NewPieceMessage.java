8bd3f02380bea9b155390c2e11db66bf51037c71
Student <Student@192.168.117.126>
2013-06-18 21:35:22 -0400

    tetrisClient log messages and implements discoverServer

--- src/edu/touro/cooptetris/net/message/NewPieceMessage.java
+++ src/edu/touro/cooptetris/net/message/NewPieceMessage.java
@@ -1,10 +1,14 @@
 package edu.touro.cooptetris.net.message;
 
+import java.util.logging.Logger;
+
 import edu.touro.cooptetris.net.client.ClientGameController;
 import edu.touro.cooptetris.net.server.ServerGameController;
 import edu.touro.cooptetris.pieces.Piece;
 
 public class NewPieceMessage implements Message {
+	private final static Logger logger = Logger.getLogger(NewPieceMessage.class
+			.getName());
 
 	private static final long serialVersionUID = 1L;
 	private Piece piece;
@@ -15,6 +19,8 @@ public class NewPieceMessage implements Message {
 
 	@Override
 	public void handleByClient(ClientGameController gameController) {
+
+		logger.info("received new piece message");
 		gameController.addNewPiece(piece);
 	}
 
